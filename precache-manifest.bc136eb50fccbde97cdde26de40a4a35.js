20aa1035d7103606fc992298e75bff4d82a0cc21
James Zuckerman <jzuckerman4@gmail.com>
2020-08-09 16:04:25 -0700

    Updates

---
+++ precache-manifest.bc136eb50fccbde97cdde26de40a4a35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e9b1c7da9ad14cd458dbf89021c4a8f",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "81e06dee3d27c16c04c1",
+    "url": "/covid-19-tracker/static/css/main.1bf14ddb.chunk.css"
+  },
+  {
+    "revision": "09c2f67adfe3d47786f3",
+    "url": "/covid-19-tracker/static/js/2.13121c06.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid-19-tracker/static/js/2.13121c06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81e06dee3d27c16c04c1",
+    "url": "/covid-19-tracker/static/js/main.ab32390a.chunk.js"
+  },
+  {
+    "revision": "aa1b0a0524add1736f5e",
+    "url": "/covid-19-tracker/static/js/runtime-main.96b4abf0.js"
+  }
+]);
